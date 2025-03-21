35dfe6ca5d6ffdbd921a8138e05ea8e789405929
JustZi02 <LizavetaIvanovskaya@gmail.com>
2025-03-21 19:58:42 +0300

    Tests for Cart Service added

---
+++ src/test/java/com/es/phoneshop/model/cart/DefaultCartServiceTest.java
@@ -0,0 +1,48 @@
+package com.es.phoneshop.model.cart;
+
+import com.es.phoneshop.model.exceptions.OutOfStockException;
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.Product;
+import com.es.phoneshop.model.product.ProductDao;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DefaultCartServiceTest {
+    private CartService cartService;
+    private Cart cart;
+    private ProductDao productDao;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    String CART_SESSION_ATTRIBUTE;
+
+    @Before
+    public void setup() {
+        cartService = DefaultCartService.getInstance();
+        cart = new Cart();
+        productDao = ArrayListProductDao.getInstance();
+        Currency usd = Currency.getInstance("USD");
+        productDao.save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg"));
+        productDao.save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg"));
+        productDao.save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+    }
+
+    @Test(expected = OutOfStockException.class)
+    public void AddToCartIncorrectQuantity() {
+        cartService.add(cart, 0l, 1000);
+    }
+
+    @Test
+    public void AddToCartCorrectQuantity() {
+        cartService.add(cart, 0l, 1);
+        assertThat(cart, notNullValue());
+    }
+}
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -22,8 +22,6 @@ public class ArrayListProductDaoTest {
         productDao.save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg"));
         productDao.save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg"));
         productDao.save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
-
-
     }
 
     @Test
